25013b6c2a45a29ac93a091c23e94a4655da2c88
Martin Meixger <mail@meixger.com>
2022-12-04 21:21:36 +0100

    day1: solution

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent Of ~~Vue~~ React 2022
 
 This repository contains my solutions for the [Advent of VUE 2022](https://adventofvue.com/) but in React.
+
+- Day1: Search products
--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,10 +1,53 @@
-const findProducts = async term => { }
+import { useState, useEffect } from "react";
+import { useDebounce } from "./debounce";
+
+const findProducts = async term => {
+  const url = 'https://dummyjson.com/products/search?limit=20&select=id,title,price&q=' + term;
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => json.products)
+    .catch(err => {
+      alert(err);
+    });
+}
 
 function App() {
-  
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState();
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    findProducts(debouncedSearchTerm)
+      .then(_ => {
+        setResult(_);
+      })
+    setLoading(false);
+  }, [debouncedSearchTerm]);
+
+  const onInputChange = (e) => {
+    setLoading(true);
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="container m-auto">
-      <h1 className="grid h-screen place-items-center">TODO</h1>
+    <div className="App">
+      <div className="w-full h-full flex flex-col gap-5 justify-center items-center">
+        <h1 className="text-4xl font-bold">Gift Search Bar</h1>
+        <div className="flex items-center">
+          <input type="text" className="p-2 border-2 border-gray-dark" placeholder="Start typing..." onChange={onInputChange} />
+        </div>
+        <ul className="list-disc">
+          {loading
+            ? (<li>loading...</li>)
+            : !(result?.length > 0)
+              ? (<li>No Result</li>)
+              : result.map(_ =>
+                (<li key={_.id}>{`${_.title} ${_.price}$`}</li>)
+              )
+          }
+        </ul>
+      </div>
     </div>
   )
 }
