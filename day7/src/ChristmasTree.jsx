ed1ac559f2b55a09da112aff658c96f792d4880a
Martin Meixger <mail@meixger.com>
2022-12-08 20:55:39 +0100

    day8: challenge

---
+++ day7/src/ChristmasTree.jsx
@@ -0,0 +1,13 @@
+import { ReactComponent as Tree } from "./assets/tree.svg"
+import ChristmasPresent from "./ChristmasPresent"
+export default function ChristmasTree({ presents }) {
+
+  return (
+    <div className="relative flex justify-center">
+      <Tree className="w-64" />
+      <div className="flex items-end absolute -bottom-8 -gap-8 presents" data-v-auto-animate>
+        {presents.map(p => <ChristmasPresent key={p} name={p} />)}
+      </div>
+    </div>
+  )
+}
