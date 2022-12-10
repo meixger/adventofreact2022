a466b8d3caa20c248d3a95dac0d38872b8c1b8bf
Martin Meixger <mail@meixger.com>
2022-12-10 19:08:16 +0100

    day8: refactor from tailwindcss to unocss

--- day8/vite.config.js
+++ day8/vite.config.js
@@ -1,7 +1,43 @@
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import Unocss from "unocss/vite";
+import presetWind from "@unocss/preset-wind";
+import presetIcons from "@unocss/preset-icons";
+import { presetWebFonts } from "unocss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    Unocss({
+      presets: [
+        presetWind(),
+        presetIcons({
+          extraProperties: {
+            display: "inline-block",
+            "vertical-align": "middle",
+          },
+        }),
+        presetWebFonts({
+          fonts: {
+            mono: ["Fira Code"],
+            serif: ["Noto Serif"],
+            sans: ["Noto Sans"],
+          },
+        }),
+      ],
+      extendTheme: (theme) => ({
+        ...theme,
+        colors: {
+          ...theme.colors,
+          green: "#42b883",
+        },
+      }),
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
