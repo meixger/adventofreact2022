163ebcc6b0459bf03b499dd5952507a9bdc23850
Martin Meixger <mail@meixger.com>
2022-12-11 11:42:00 +0100

    day9: challenge

--- day9/src/App.jsx
+++ day9/src/App.jsx
@@ -1,6 +1,24 @@
+import presents from "./presents.json";
+
 function App() {
   return (
-    <h1 className="grid h-screen place-items-center">TODO</h1>
+    <div className="w-full h-full p-4 flex justify-center items-center">
+      <div className="max-w-md">
+        <div>
+          <img src="/assets/tree.svg" alt="Christmas tree" />
+        </div>
+        <div className="mt-2 flex justify-center items-center">
+          {presents.map((p) => (
+            <img
+              key={p.id}
+              src={p.src}
+              alt={`Present ${p.id}`}
+              data-qa="present"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ day9/src/App.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen, configure, fireEvent } from "@testing-library/react";
+import { it, expect } from "vitest";
+
+import App from "./App.jsx";
+
+configure({ testIdAttribute: "data-qa" });
+
+it("should sort presents from small to big by dimensions when toggling sort", async () => {
+  render(<App />);
+  console.log(screen.logTestingPlaygroundURL());
+
+  await fireEvent.click(
+    await screen.findByRole("button", { name: "Toggle sort" })
+  );
+
+  const presents = await screen.findAllByTestId("present");
+  expect(presents[0].getAttribute("alt")).toBe("Present 2");
+  expect(presents[1].getAttribute("alt")).toBe("Present 1");
+  expect(presents[2].getAttribute("alt")).toBe("Present 3");
+  expect(presents[3].getAttribute("alt")).toBe("Present 5");
+  expect(presents[4].getAttribute("alt")).toBe("Present 4");
+});
